b374b90e039d01d42950449fe02d765a31d26d58
Mr.Stalin <Fakels@yandex.ru>
2021-06-16 21:21:46 +0300

    Upload missing class file.

---
+++ ProtoManager/Classes/ExtraMod.vb
@@ -0,0 +1,36 @@
+﻿Public Class ExtraModData
+
+    Public filePath As String
+    Public fileName As String
+    Public isDat As Boolean
+    Public isEnabled As Boolean = False
+
+    Sub New(ByRef modPath As String, isDat As Boolean)
+        Me.filePath = modPath
+        Me.isDat = isDat
+
+        If (isDat) Then SetDatName()
+    End Sub
+
+    Sub New(ByRef modPath As String, isDat As Boolean, isEnabled As Boolean)
+        Me.filePath = modPath
+        Me.isDat = isDat
+        Me.isEnabled = isEnabled
+
+        If (isDat) Then SetDatName()
+    End Sub
+
+    Sub SetDatName()
+        fileName = IO.Path.GetFileName(filePath)
+    End Sub
+
+End Class
+
+Public Class ExtraModComparer
+    Implements IComparer(Of ExtraModData)
+
+    Public Function Compare(a As ExtraModData, b As ExtraModData) As Integer Implements IComparer(Of ExtraModData).Compare
+        Return String.Compare(a.filePath, b.filePath)
+    End Function
+End Class
+
